af55500ec4299675e3ca030e08533cb8e7971512
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-29 23:49:13 +0200

    Sidebar működésbe hozása
    script megírása + 1sec animáció -> 0.7sec

---
+++ assets/js/min/app-min.js
@@ -0,0 +1,1 @@
+$(".turn-sidebar-off").click(function(){$(".sidebar").toggleClass("sidebar-hidden"),$(".turn-sidebar-on").toggleClass("hidden")}),$(".turn-sidebar-on").click(function(){$(".sidebar").toggleClass("sidebar-hidden"),$(".turn-sidebar-on").toggleClass("hidden")});
