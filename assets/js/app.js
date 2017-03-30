630835a7328fe314a34403af4f39f4e6d670d5d8
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-30 21:25:57 +0200

    Kérdések objektum csontváza
    kérdés, helyes válasz, három helytelen válasz, jutalom

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,11 @@ $(".turn-sidebar-on").click(function() {
 	$(".sidebar").toggleClass("sidebar-hidden");
 	$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
 });
+var questions = [{
+	question: "Melyik város az európai únió központja?",
+	correct: "Brüsszel",
+	unCorrect1: "Párizs",
+	unCorrect2: "London",
+	unCorrect3: "Berlin",
+	reward: 34,
+}];
