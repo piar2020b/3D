63d6e4f800e07182fc35910c71821cada69d1c16
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-30 06:46:03 +0200

    Sidebar animáció fejlesztése

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,8 @@
 $(".turn-sidebar-off").click(function() {
 	$(".sidebar").toggleClass("sidebar-hidden");
-	$(".turn-sidebar-on").toggleClass("hidden");
+	$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
 });
 $(".turn-sidebar-on").click(function() {
 	$(".sidebar").toggleClass("sidebar-hidden");
-	$(".turn-sidebar-on").toggleClass("hidden");
+	$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
 });
