bdb5ebd9f35e28bb6055990a73156e5d1a0a1e7b
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-30 15:35:31 +0200

    Animációk javítása
    Már nem buggol a scrollbar, mikor a sidebart ki- és bekapcsoljuk.

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,9 @@ $(".turn-sidebar-on").click(function() {
 	$(".sidebar").toggleClass("sidebar-hidden");
 	$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
 });
+$(".question").click(function() {
+	if (!$(".sidebar").hasClass("sidebar-hidden")) {
+		$(".sidebar").toggleClass("sidebar-hidden");
+		$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
+	}
+});
