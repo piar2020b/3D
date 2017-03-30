32aeb4140c0daeef5d4d337aa3cf0abd55c6dff6
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-30 15:59:57 +0200

    Árnyék effekt

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,3 @@ $(".turn-sidebar-on").click(function() {
 	$(".sidebar").toggleClass("sidebar-hidden");
 	$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
 });
-$(".question").click(function() {
-	if (!$(".sidebar").hasClass("sidebar-hidden")) {
-		$(".sidebar").toggleClass("sidebar-hidden");
-		$(".turn-sidebar-on").toggleClass("sidebar-turn-hidden");
-	}
-});
