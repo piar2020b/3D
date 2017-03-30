d1747c6484ed07a616edcbb3b645f0ac760d2e42
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-03-30 22:01:12 +0200

    Kijelző funkció
    Kijelzi az adott sorszámú kérdést, a válaszlehetőségek sorrendje véletlenszerű.

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,32 @@ var questions = [{
 	unCorrect3: "Berlin",
 	reward: 34,
 }];
+var display = function(nth) {
+	$('.question').text(questions[nth].question);
+	switch (Math.floor(Math.random() * 4) + 1) {
+		case 1:
+			$('#first-answer').text(questions[nth].correct);
+			$('#second-answer').text(questions[nth].unCorrect1);
+			$('#third-answer').text(questions[nth].unCorrect2);
+			$('#fourth-answer').text(questions[nth].unCorrect3);
+			break;
+		case 2:
+			$('#second-answer').text(questions[nth].correct);
+			$('#first-answer').text(questions[nth].unCorrect1);
+			$('#third-answer').text(questions[nth].unCorrect2);
+			$('#fourth-answer').text(questions[nth].unCorrect3);
+			break;
+		case 3:
+			$('#third-answer').text(questions[nth].correct);
+			$('#second-answer').text(questions[nth].unCorrect1);
+			$('#first-answer').text(questions[nth].unCorrect2);
+			$('#fourth-answer').text(questions[nth].unCorrect3);
+			break;
+		case 4:
+			$('#fourth-answer').text(questions[nth].correct);
+			$('#second-answer').text(questions[nth].unCorrect1);
+			$('#third-answer').text(questions[nth].unCorrect2);
+			$('#first-answer').text(questions[nth].unCorrect3);
+			break;
+	}
+};
