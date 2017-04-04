de0343c70dd00e1ca9b928a066ba9aa36db940aa
Markus Ivancsics <ivancsics.markus@icloud.com>
2017-04-04 21:22:28 +0200

    Oldalszámok hozzáadása:
    Az utolsó ~20 kérdésnél nem volt meg az oldalszám, pótoltam.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -669,149 +669,173 @@ var questions = [{
 	"inCorrect1": "Párizs",
 	"inCorrect2": "London",
 	"inCorrect3": "Berlin",
-	"reward": "30"
+	"page": '132'
 }, {
 	"question": "Hány nemzet vezetője írta alá az első Európai Uniót alapító szerződést?",
 	"correct": "6",
 	"inCorrect1": "4",
 	"inCorrect2": "5",
-	"inCorrect3": "3"
+	"inCorrect3": "3",
+	"page": '132'
 }, {
 	'question': 'Hány nemzet vezetője írta alá az első európai úniót alapító szerződést?',
 	'correct': '6',
 	'inCorrect1': '4',
 	'inCorrect2': '5',
-	'inCorrect3': '3'
+	'inCorrect3': '3',
+	"page": '132'
 }, {
 	'question': 'Hol írták alá az első, európai Közösséget megalapító szerződést?',
 	'correct': 'Róma',
 	'inCorrect1': 'Párizs',
 	'inCorrect2': 'Krakkó',
-	'inCorrect3': 'Berlin'
+	'inCorrect3': 'Berlin',
+	"page": '132'
 }, {
 	'question': 'Mi annak az épületnek a neve, amely az európai únió székhelye?',
 	'correct': 'Berlaymont',
 	'inCorrect1': 'Kreml',
 	'inCorrect2': 'Conciergeire',
-	'inCorrect3': 'Riksdag'
+	'inCorrect3': 'Riksdag',
+	"page": '132'
 }, {
 	'question': 'Hány tagállam van az európai únióban?',
 	'correct': '28',
 	'inCorrect1': '25',
 	'inCorrect2': '26',
-	'inCorrect3': '27'
+	'inCorrect3': '27',
+	"page": '132'
 }, {
 	'question': 'Mennyi az európai únió össznépessége?',
 	'correct': '500 millió',
 	'inCorrect1': '450 millió',
 	'inCorrect2': '1 milliárd',
-	'inCorrect3': '800 millió'
+	'inCorrect3': '800 millió',
+	"page": '132'
 }, {
 	'question': 'A  közvetlen külföldi tőkebefektetéseknek hány %-a kötődik az eu-hoz?',
 	'correct': '0,2',
 	'inCorrect1': '0,4',
 	'inCorrect2': '0,05',
-	'inCorrect3': '0,5'
+	'inCorrect3': '0,5',
+	"page": '132'
 }, {
 	'question': 'Melyik évben következett be a világgazdasági válság?',
 	'correct': '2008',
 	'inCorrect1': '2010',
 	'inCorrect2': '1990',
-	'inCorrect3': '2000'
+	'inCorrect3': '2000',
+	"page": '132'
 }, {
 	'question': 'Az európai együttműködésben milyen tényezők játszottak szerepet?',
 	'correct': 'Politika és gazdaság',
 	'inCorrect1': 'Gazdaság',
 	'inCorrect2': 'Politika ',
-	'inCorrect3': 'Hadászat'
+	'inCorrect3': 'Hadászat',
+	"page": '133'
 }, {
 	'question': 'Svájcnak milyn státusza van?',
 	'correct': 'nem EU-tag, schengeni ország',
 	'inCorrect1': 'EU-tag, schengeni ország',
 	'inCorrect2': 'Nem EU-tag, nem schengeni ország',
-	'inCorrect3': 'EU-tag, nem schengeni ország'
+	'inCorrect3': 'EU-tag, nem schengeni ország',
+	"page": '133'
 }, {
 	'question': 'Angliának milyen státusza van?',
 	'correct': 'EU-tag, nem schengeni ország',
 	'inCorrect1': 'EU-tag, schengeni ország',
 	'inCorrect2': 'Nem EU-tag, nem schengeni ország',
-	'inCorrect3': 'Nem EU-tag, schengeni ország'
+	'inCorrect3': 'Nem EU-tag, schengeni ország',
+	"page": '133'
 }, {
 	'question': 'Romániának milyen státusza van?',
 	'correct': 'EU-tag, nem teljes jogú schengeni ország',
 	'inCorrect1': 'Eu-tag, nem schengeni ország',
 	'inCorrect2': 'Nem EU-tag, nem schengeni ország',
-	'inCorrect3': 'Eu-tag, schengeni ország'
+	'inCorrect3': 'Eu-tag, schengeni ország',
+	"page": '133'
 }, {
 	'question': 'Magyarországnak milyen státusza van?',
 	'correct': 'EU-tag, schengeni ország',
 	'inCorrect1': 'Eu-tag, nem schengeni ország',
 	'inCorrect2': 'Nem EU-tag, nem schengeni ország',
-	'inCorrect3': 'Nem EU-tag , schengeni ország'
+	'inCorrect3': 'Nem EU-tag , schengeni ország',
+	"page": '133'
 }, {
 	'question': 'Mi az az EGK?',
 	'correct': 'Európai gazdasági közösség',
 	'inCorrect1': 'Európai gazdasági közppont',
 	'inCorrect2': 'Energetikai gazdasági kereskedők',
-	'inCorrect3': 'Egyetemes genetikai közösség'
+	'inCorrect3': 'Egyetemes genetikai közösség',
+	"page": '133'
 }, {
 	'question': 'Mióta nincsenek határok?',
 	'correct': '1992',
 	'inCorrect1': '1993',
 	'inCorrect2': '1990',
-	'inCorrect3': '1991'
+	'inCorrect3': '1991',
+	"page": '133'
 }, {
 	'question': 'Milyen szerződést írtak alá 1992-ben?',
 	'correct': 'Maastrichti',
 	'inCorrect1': 'Párizsi',
 	'inCorrect2': 'Krakkói',
-	'inCorrect3': 'Berlin'
+	'inCorrect3': 'Berlin',
+	"page": '134'
 }, {
 	'question': 'Hány tagállamban használnak eurót?',
 	'correct': '19',
 	'inCorrect1': '20',
 	'inCorrect2': '18',
-	'inCorrect3': '15'
+	'inCorrect3': '15',
+	"page": '134'
 }, {
 	'question': 'Milyen szerződést írtak alá 2009-ben?',
 	'correct': 'Liszaboni',
 	'inCorrect1': 'Madridi',
 	'inCorrect2': 'Stockholmi',
-	'inCorrect3': 'Bécsi'
+	'inCorrect3': 'Bécsi',
+	"page": '134'
 }, {
 	'question': 'Melyik szerződéseket módosította a Liszaboni szerződés?',
 	'correct': 'Maastrichtit és a Rómait',
 	'inCorrect1': 'Krakkóit és a Rómait',
 	'inCorrect2': 'Maastrichtit és a Trianonit',
-	'inCorrect3': 'Krakkóit és a Trianonit'
+	'inCorrect3': 'Krakkóit és a Trianonit',
+	"page": '134'
 }, {
 	'question': 'Mikor kezdődött a közös agrárpolitika?',
 	'correct': '1960',
 	'inCorrect1': '1970',
 	'inCorrect2': '2000',
-	'inCorrect3': '1990'
+	'inCorrect3': '1990',
+	"page": '136'
 }, {
 	'question': 'Milyen céllal kezdődött az agrárpolitika?',
 	'correct': 'Nyugat-európai országok élelmiszer ellátása biztos legyen',
 	'inCorrect1': 'Egységes európai tanács létrejötte',
 	'inCorrect2': 'A határok megszűntetése az EU-n belül, ezáltal egységesebbé tenni az úniót',
-	'inCorrect3': 'Az országok egységes irányítása egy törvénykönyv alapján'
+	'inCorrect3': 'Az országok egységes irányítása egy törvénykönyv alapján',
+	"page": '136'
 }, {
 	'question': 'Melyik területet nevezzük az ipari forradalom bölcsőjének?',
 	'correct': 'Közép-Angliát',
 	'inCorrect1': 'Észak-Németországot',
 	'inCorrect2': 'Észak-Franciaországot',
-	'inCorrect3': 'Észa-Olaszországot'
+	'inCorrect3': 'Észak-Olaszországot',
+	"page": '138'
 }, {
 	'question': 'Melyik három város alkotja az olasz ipari háromszöget?',
 	'correct': 'Torino-Milano-Genova',
 	'inCorrect1': 'Torino-Róma-Verona',
 	'inCorrect2': 'Milánó-Udine-Ravenna',
-	'inCorrect3': 'Torino-Róma-Milánó'
+	'inCorrect3': 'Torino-Róma-Milánó',
+	"page": '138'
 }, {
 	'question': 'Hogy nevezzük a közép-Angliától az olasz ipari háromszig tartó európai gazdasági központot?',
 	'correct': 'Kék banán',
 	'inCorrect1': 'Zöld ív',
 	'inCorrect2': 'Gazdasági tengely',
-	'inCorrect3': 'Termékeny félhold'
+	'inCorrect3': 'Termékeny félhold',
+	"page": '138'
 }];
